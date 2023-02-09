d1368903a60dadc8ee74241b13d0d1d768adb4bb
Alex Kiselich <alexander.kiselich@gmail.com>
2023-02-08 18:23:07 -0700

    Add best friends

---
+++ friends.rb
@@ -0,0 +1,10 @@
+best_friend1 = "Javen"
+best_friend2 = "Mike"
+
+if best_friend1 == "Javen"
+    puts "What's up my homie!"
+elsif best_friend2 == "Mike"
+    puts "Cheers mate!"
+else 
+    puts "Hey you are not my friend!"
+end
